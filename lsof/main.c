7a3c8cc09cac86115ff2700aeea265d7dc8f90a6
Akimov Kirill <akimowkirill@gmail.com>
2018-11-11 17:04:42 +0300

    working with errno fixed, overflow of buffer fixed

--- lsof/main.c
+++ lsof/main.c
@@ -11,7 +11,7 @@
 const char directory_proc[] = "/proc/";
 
 int main() {
-    errno=0;
+    errno = 0;
 
     char directory_fd[64];
     DIR *proc_dir;
@@ -25,51 +25,60 @@ int main() {
 
     proc_dir = opendir(directory_proc);
     if (!proc_dir) {
+        printf("Can't open %s", directory_proc);
         perror("proc_dir_open");
         exit(1);
     };
     while ((proc_entry = readdir(proc_dir)) != NULL) {
-        if(proc_entry==NULL){
-            perror("readdir");
-            exit(1);
-        };
         if (!isdigit(proc_entry->d_name[0]))
             continue;
         snprintf(directory_fd, sizeof(directory_fd), "/proc/%s/fd/", proc_entry->d_name);
 
         fd_dir = opendir(directory_fd);
-        if (fd_dir) {
-            while ((fd_entry = readdir(fd_dir)) != NULL) {
-                if (!isdigit(fd_entry->d_name[0]))
-                    continue;
-                snprintf(directory_fd, sizeof(directory_fd), "/proc/%s/fd/%s", proc_entry->d_name, fd_entry->d_name);
-                lstat(directory_fd, &status_buffer);
+        if (!fd_dir) {
+            printf("Can't open %s\n", directory_fd);
+            perror("proc_dir_open");
+            errno = 0;
+            continue;
+        };
 
-                if (S_ISDIR(status_buffer.st_mode))
-                    continue;
-                if (status_buffer.st_uid != getuid())
-                    continue;
-
-                unsigned int buffer_size = 1024;
-                char *buffer = calloc(buffer_size, sizeof(char));//calloc makes string null-terminated
+        while ((fd_entry = readdir(fd_dir)) != NULL) {
+            if (!isdigit(fd_entry->d_name[0]))
+                continue;
+            snprintf(directory_fd, sizeof(directory_fd), "/proc/%s/fd/%s", proc_entry->d_name, fd_entry->d_name);
+            if (lstat(directory_fd, &status_buffer) == -1) {
+                printf("Error in lstat in %s\n", directory_fd);
+                perror("lstat:");
+                errno = 0;
+                continue;
+            }
+            if (S_ISDIR(status_buffer.st_mode))
+                continue;
+            if (status_buffer.st_uid != getuid())
+                continue;
+            unsigned int buffer_size = 1024;
+            char *buffer = (char *) calloc(buffer_size, 1);//calloc makes string null-terminated
+            readlink(directory_fd, buffer, buffer_size);
+            while (buffer[buffer_size - 1] != 0) {
+                buffer_size *= 2;
+                buffer = (char *) realloc(buffer, buffer_size);
                 readlink(directory_fd, buffer, buffer_size);
-                printf("[pid] - %s, opened file - %s\n", proc_entry->d_name, buffer);
-                free(buffer);
             }
-            if (errno != 0){
-                printf("Error in reading dir /proc/%s/fd/\n",proc_entry->d_name);
-                perror("readdir");
-//                exit(1);
-            }
-
+            printf("[pid] - %s, opened file - %s\n", proc_entry->d_name, buffer);
+            free(buffer);
         }
+        if (errno != 0) {
+            printf("Error in reading dir %s\n", fd_dir);
+            perror("Readdir");
+            errno = 0;
+        }
+
         closedir(fd_dir);
     }
-
-    if (errno != 0){
-        printf("Error in reading dir /proc/\n");
-        perror("readdir");
-//        exit(1);
+    if (errno != 0) {
+        printf("Error in reading dir %s\n", proc_dir);
+        perror("Readdir");
+        errno = 0;
     }
     closedir(proc_dir);
     return 0;
